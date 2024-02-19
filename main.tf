a906b807cc83daf97b59a2d9a5ca187da2f0fe74
Diego Gomes <diego.gomes.191287@gmail.com>
2024-02-19 14:45:15 -0300

    fixed our pipeline

--- main.tf
+++ main.tf
@@ -22,6 +22,12 @@ terraform {
     }
 }
 
+variable "imagebuild" {
+  type        = string
+  description = "Latest Image Build"
+}
+
+
 resource "azurerm_resource_group" "tf_test" {
   name = "tfmainrg"
   location = "East US"
@@ -38,7 +44,7 @@ resource "azurerm_container_group" "tfcg_test" {
 
   container {
       name            = "weatherapi-container"
-      image           = "diegoslim/weatherapi:latest"
+      image           = "diegoslim/weatherapi:${var.imagebuild}"
         cpu             = "1"
         memory          = "1"
 
